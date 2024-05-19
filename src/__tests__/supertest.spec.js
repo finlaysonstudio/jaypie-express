eccfccef73febb14366d9d78ba1c958d3c8a3fb2
Adam Finlayson <amf@adamfinlayson.com>
2024-05-18 23:12:30 -0500

    test: post requests

--- src/__tests__/supertest.spec.js
+++ src/__tests__/supertest.spec.js
@@ -61,5 +61,32 @@ describe("Project handler function", () => {
       expect(Object.keys(log.info.var.mock.calls[1][0]).length).toEqual(1);
       expect(decorateResponse).toBeCalledTimes(1);
     });
+    it("POST requests with a body", async () => {
+      // Set up our mock function
+      const mockFunction = vi.fn(() => ({
+        goose: "honk",
+      }));
+      const handler = expressHandler(mockFunction, {
+        name: "handler",
+      });
+      // Set up our mock express app
+      const app = express();
+      app.use(express.json());
+      app.use(handler);
+      // Make a request
+      const res = await request(app).post("/").send({ cat: "meow" });
+      expect(res.body).toEqual({ goose: "honk" });
+      // Check the log was called twice: once for the request, once for the response
+      expect(log.var).toBeCalledTimes(2);
+      // Both calls should be an object with a single key: "req" or "res"
+      expect(log.info.var.mock.calls[0][0]).toHaveProperty("req");
+      expect(log.info.var.mock.calls[0][0].req.body).toEqual({ cat: "meow" });
+      expect(log.info.var.mock.calls[1][0]).toHaveProperty("res");
+      expect(log.info.var.mock.calls[1][0].res.body).toEqual({ goose: "honk" });
+      // The count of keys in each call should be 1
+      expect(Object.keys(log.info.var.mock.calls[0][0]).length).toEqual(1);
+      expect(Object.keys(log.info.var.mock.calls[1][0]).length).toEqual(1);
+      expect(decorateResponse).toBeCalledTimes(1);
+    });
   });
 });
