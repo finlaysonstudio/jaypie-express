7f5c314cda0e2c09a16a98f8b53f8126d627599a
Adam Finlayson <amf@adamfinlayson.com>
2024-05-21 20:37:47 -0500

    test: route responses

--- src/__tests__/supertest.spec.js
+++ src/__tests__/supertest.spec.js
@@ -134,6 +134,29 @@ describe("Express handler", () => {
         expect(Object.keys(log.info.var.mock.calls[0][0]).length).toEqual(1);
         expect(Object.keys(log.info.var.mock.calls[1][0]).length).toEqual(1);
       });
+      it("Returning String", async () => {
+        // Set up our mock function
+        const mockFunction = vi.fn(() => "Hello, world!");
+        const handler = expressHandler(mockFunction, {
+          name: "handler",
+        });
+        // Set up our mock express app
+        const app = express();
+        app.use(handler);
+        // Make a request
+        const res = await request(app).get("/");
+        expect(res.headers["content-type"]).toContain("text/html");
+        expect(res.text).toEqual("Hello, world!");
+        // Check the log was called twice: once for the request, once for the response
+        expect(log.var).toBeCalledTimes(2);
+        // Both calls should be an object with a single key: "req" or "res"
+        expect(log.info.var.mock.calls[0][0]).toHaveProperty("req");
+        expect(log.info.var.mock.calls[1][0]).toHaveProperty("res");
+        expect(log.info.var.mock.calls[1][0].res.body).toEqual("Hello, world!");
+        // The count of keys in each call should be 1
+        expect(Object.keys(log.info.var.mock.calls[0][0]).length).toEqual(1);
+        expect(Object.keys(log.info.var.mock.calls[1][0]).length).toEqual(1);
+      });
       it("Returning no content", async () => {
         const mockFunction = vi.fn(() => null);
         const handler = expressHandler(mockFunction, {
@@ -144,6 +167,7 @@ describe("Express handler", () => {
         // Make a request
         const res = await request(app).get("/");
         expect(res.status).toEqual(HTTP.CODE.NO_CONTENT);
+        expect(res.headers["content-type"]).toBeUndefined();
       });
       it("Returning created", async () => {
         const mockFunction = vi.fn(() => true);
@@ -155,6 +179,7 @@ describe("Express handler", () => {
         // Make a request
         const res = await request(app).get("/");
         expect(res.status).toEqual(HTTP.CODE.CREATED);
+        expect(res.headers["content-type"]).toBeUndefined();
       });
     });
   });
