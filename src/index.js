82fb00354fcb8a4ed85995cc70da5cc1bba5c453
Adam Finlayson <amf@adamfinlayson.com>
2024-05-21 19:12:51 -0500

    chore: remove: echo handler; rename: http handler

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 export { EXPRESS } from "./constants.js";
-export { default as expressEchoHandler } from "./echo.handler.js";
 export { default as expressHandler } from "./expressHandler.js";
-export { default as expressHttpHandler } from "./http.handler.js";
+export { default as expressHttpCodeHandler } from "./http.handler.js";
