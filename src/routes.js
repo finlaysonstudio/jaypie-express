18ad3a092a59a9a1aff0d1bc93e6a8ab38a1c4f0
Adam Finlayson <amf@adamfinlayson.com>
2024-07-05 20:06:29 -0500

    feat: badRequestRoute, methodNotAllowedRoute

--- src/routes.js
+++ src/routes.js
@@ -9,9 +9,13 @@ import echoHandler from "./echo.handler.js";
 //
 
 const routes = {
+  badRequestRoute: httpHandler(HTTP.CODE.BAD_REQUEST, { name: "_badRequest" }),
   echoRoute: echoHandler(),
   forbiddenRoute: httpHandler(HTTP.CODE.FORBIDDEN, { name: "_forbidden" }),
   goneRoute: httpHandler(HTTP.CODE.GONE, { name: "_gone" }),
+  methodNotAllowedRoute: httpHandler(HTTP.CODE.METHOD_NOT_ALLOWED, {
+    name: "_methodNotAllowed",
+  }),
   noContentRoute: httpHandler(HTTP.CODE.NO_CONTENT, { name: "_noContent" }),
   notFoundRoute: httpHandler(HTTP.CODE.NOT_FOUND, { name: "_notFound" }),
   notImplementedRoute: expressHandler(
@@ -28,9 +32,11 @@ const routes = {
 //
 
 export const {
+  badRequestRoute,
   echoRoute,
   forbiddenRoute,
   goneRoute,
+  methodNotAllowedRoute,
   noContentRoute,
   notFoundRoute,
   notImplementedRoute,
